c8ae69da263562ba8c23ec1970a698abdb2e8be8
hikitani <kazbekmamakaev@mail.ru>
2024-07-25 18:00:54 +0300

    feat: add package constructor

--- foo_test.go
+++ foo_test.go
@@ -10,13 +10,16 @@ import (
 func TestFoo(t *testing.T) {
 	vm := New()
 	stmt, err := vm.Compile(strings.NewReader(`
-		res = range(10)
+		res = range(10, 100, 3)
 			.where(|v| => v % 2 == 0)
 			.select(|v| => v * 2)
-			.max(2)
+			.max(10)
 			.list()
 
-		println(res)
+		obj = {
+			"arr": res,
+		}
+		println(obj)
 	`))
 	require.NoError(t, err)
 
--- package.go
+++ package.go
@@ -2,6 +2,7 @@ package easylang
 
 import (
 	"errors"
+	"math/big"
 
 	"github.com/hikitani/easylang/builtin"
 	"github.com/hikitani/easylang/variant"
@@ -13,7 +14,7 @@ type PackageRegister struct {
 
 func (reg *PackageRegister) Register(pkg Package) error {
 	if pkg.Name() == "builtin" {
-		if _, ok := pkg.(*builtinPkg); !ok {
+		if pkg != builtinPkg {
 			return errors.New("package name 'builtin' is reserved")
 		}
 
@@ -31,8 +32,113 @@ func (reg *PackageRegister) Register(pkg Package) error {
 func NewPackageRegister() *PackageRegister {
 	return &PackageRegister{
 		packages: map[string]Package{
-			"builtin": &builtinPkg{},
+			builtinPkg.Name(): builtinPkg,
 		},
+	}
+}
+
+type PackageConstructor struct {
+	name    string
+	objects map[string]variant.Iface
+}
+
+func (p *PackageConstructor) AddVariant(name string, obj variant.Iface) *PackageConstructor {
+	p.objects[name] = obj
+	return p
+}
+
+func (p *PackageConstructor) AddNone(name string) *PackageConstructor {
+	return p.AddVariant(name, variant.NewNone())
+}
+
+func (p *PackageConstructor) AddBool(name string, v bool) *PackageConstructor {
+	return p.AddVariant(name, variant.NewBool(v))
+}
+
+func (p *PackageConstructor) AddInt(name string, v int) *PackageConstructor {
+	return p.AddVariant(name, variant.Int(v))
+}
+
+func (p *PackageConstructor) AddUInt(name string, v int) *PackageConstructor {
+	return p.AddVariant(name, variant.Int(v))
+}
+
+func (p *PackageConstructor) AddFloat(name string, v float64) *PackageConstructor {
+	return p.AddVariant(name, variant.Float(v))
+}
+
+func (p *PackageConstructor) AddInf(name string) *PackageConstructor {
+	return p.AddVariant(name, variant.Inf())
+}
+
+func (p *PackageConstructor) AddNegInf(name string) *PackageConstructor {
+	return p.AddVariant(name, variant.NegInf())
+}
+
+func (p *PackageConstructor) AddBigFloat(name string, v *big.Float) *PackageConstructor {
+	return p.AddVariant(name, variant.NewNum(v))
+}
+
+func (p *PackageConstructor) AddBigInt(name string, v *big.Int) *PackageConstructor {
+	return p.AddVariant(name, variant.NewNum(new(big.Float).SetInt(v)))
+}
+
+func (p *PackageConstructor) AddBigRat(name string, v *big.Rat) *PackageConstructor {
+	return p.AddVariant(name, variant.NewNum(new(big.Float).SetRat(v)))
+}
+
+func (p *PackageConstructor) AddString(name string, v string) *PackageConstructor {
+	return p.AddVariant(name, variant.NewString(v))
+}
+
+func (p *PackageConstructor) AddBytes(name string, v []byte) *PackageConstructor {
+	return p.AddVariant(name, variant.Bytes(v))
+}
+
+func (p *PackageConstructor) AddArray(name string, v []variant.Iface) *PackageConstructor {
+	return p.AddVariant(name, variant.NewArray(v))
+}
+
+func (p *PackageConstructor) AddMap(name string, v map[string]variant.Iface) *PackageConstructor {
+	keys := make([]variant.Iface, 0, len(v))
+	vals := make([]variant.Iface, 0, len(v))
+
+	for k, v := range v {
+		keys = append(keys, variant.NewString(k))
+		vals = append(vals, v)
+	}
+
+	return p.AddVariant(name, variant.MustNewObject(keys, vals))
+}
+
+func (p *PackageConstructor) AddFunc(name string, fn func(args variant.Args) (variant.Iface, error)) *PackageConstructor {
+	return p.AddVariant(name, variant.NewFunc(nil, fn))
+}
+
+func (p *PackageConstructor) AddObjects(m map[string]variant.Iface) *PackageConstructor {
+	for k, v := range m {
+		p.AddVariant(k, v)
+	}
+
+	return p
+}
+
+func (p *PackageConstructor) Name() string {
+	return p.name
+}
+
+func (p *PackageConstructor) Objects() map[string]variant.Iface {
+	return p.objects
+}
+
+func (p *PackageConstructor) Build() Package {
+	return p
+}
+
+func AddPackage(name string) *PackageConstructor {
+	return &PackageConstructor{
+		name:    name,
+		objects: map[string]variant.Iface{},
 	}
 }
 
@@ -41,16 +147,6 @@ type Package interface {
 	Objects() map[string]variant.Iface
 }
 
-var _ Package = &builtinPkg{}
-
-type builtinPkg struct{}
-
-// Name implements Package.
-func (b *builtinPkg) Name() string {
-	return "builtin"
-}
-
-// Objects implements Package.
-func (b *builtinPkg) Objects() map[string]variant.Iface {
-	return builtin.Objects()
-}
+var builtinPkg = AddPackage("builtin").
+	AddObjects(builtin.Objects()).
+	Build()
